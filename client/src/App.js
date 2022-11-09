0c853f3bc7185ae30da36d96f4fba8cceab7af22
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-11-09 13:44:53 +0530

    Define client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Welcome from "./static/Welcome"
+import Register from "./auth/authentication/Register"
+import Login from "./auth/authentication/Login"
+import Home from "./transactions/Home"
+import Visualization from "./graphs/Visualization"
+
 function App() {
   return (
     <div className="App">
       <h1>Purse</h1>
+
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/visualization" element={<Visualization />} />
+        {/* TODO: signout route */}
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
 
 /* default fonts for material ui */
@@ -13,6 +14,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
