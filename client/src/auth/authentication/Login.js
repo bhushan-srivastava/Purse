bf66c44c4721e59efb34c25cc92499a2a4c19944
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-11-09 16:17:58 +0530

    Update Login.js comments

--- client/src/auth/authentication/Login.js
+++ client/src/auth/authentication/Login.js
@@ -2,6 +2,8 @@ const Login = () => {
     return (
         <div>
             Login
+            {/* constantly keep showing login (if not logged in)
+                and show "/" if logged in */}
         </div>
     );
 }
