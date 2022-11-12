da4beda42816c123aed1461ee2ba3a7215361a09
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-11-13 03:32:51 +0530

    Add buttons to CRUD transactions

--- client/src/default_layout/DefaultLayout.js
+++ client/src/default_layout/DefaultLayout.js
@@ -1,5 +1,5 @@
-import { Layout, Dropdown, Typography } from 'antd';
-import { EditOutlined, UserDeleteOutlined, LogoutOutlined } from "@ant-design/icons"
+import { Layout, Dropdown, Typography, Button } from 'antd';
+import { EditOutlined, UserDeleteOutlined, LogoutOutlined, FilterFilled, SortAscendingOutlined, PlusOutlined, EditFilled, AreaChartOutlined, TableOutlined /*, LineChartOutlined */ } from "@ant-design/icons"
 import logo from "../static/images/logo.png"
 
 const DefaultLayout = (props) => {
@@ -50,8 +50,19 @@ const DefaultLayout = (props) => {
                     <Typography.Title>{props.children[0]}</Typography.Title>
 
                     <Layout.Header className='layout-header'>
-                        filter
-                        sort
+                        <Button type='primary' icon={<FilterFilled />} />
+
+                        <Button type='primary' icon={<SortAscendingOutlined />} />
+
+                        <Button type='primary' icon={<AreaChartOutlined />} />
+
+                        {/* <Button size='middle' type='primary' icon={<LineChartOutlined />} /> */}
+
+                        <Button type='primary' icon={<TableOutlined />} />
+
+                        <Button type='primary' icon={<EditFilled />}></Button>
+
+                        <Button type='primary' icon={<PlusOutlined />} />
                     </Layout.Header>
 
                     <Layout.Content>
