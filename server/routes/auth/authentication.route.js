9b2375eb0b1bcd5aa8441c4df40d5d08770fd6fd
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-11-17 09:05:18 +0530

    Add backend routes

--- server/routes/auth/authentication.route.js
+++ server/routes/auth/authentication.route.js
@@ -1,2 +1,23 @@
 //import controllers here
 // authentication routes
+
+import express from 'express'
+
+const router = express.Router()
+
+router.route('/api/auth/register')
+    .post(/* controller method */)
+
+router.route('/api/auth/login')
+    .post(/* controller method */)
+
+router.route('/api/auth/reset')
+    .get(/* controller method */)
+
+router.route('/api/auth/reset')
+    .post(/* controller method */)
+
+router.route('/api/auth/logout')
+    .get(/* controller method */)
+
+export default router
