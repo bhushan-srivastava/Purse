0992c408db0b5900b907e4d814e5ad28b6e5fd9a
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-10 05:48:06 +0530

    Add delete transaction controller

--- server/controllers/transactions/deleteTransaction.controller.js
+++ server/controllers/transactions/deleteTransaction.controller.js
@@ -1,1 +1,20 @@
-// TODO: deleteTransaction() - this will be the POST request function that will be passed to the express function -> app.delete('/transactions', deleteTransaction)
+import Transactions from "../../models/transactions/transaction.model.js"
+import getErrorMessages from "../errorMessages.js"
+
+async function deleteTransaction(req, res, next) {
+    try {
+        const transaction = await Transactions.findByIdAndDelete(req.params.transactionId)
+
+        if (!transaction) {
+            throw new Error('Incorrect transaction ID')
+        }
+
+        req.body.okMessage = 'Transaction deleted successfully'
+        next()
+    }
+    catch (error) {
+        res.status(400).json({ message: getErrorMessages(error) })
+    }
+}
+
+export { deleteTransaction };
