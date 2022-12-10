7470bc02d09548d199dae6f69e752696988b2375
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-10 05:46:52 +0530

    Add read transaction controller

--- server/controllers/transactions/readTransaction.controller.js
+++ server/controllers/transactions/readTransaction.controller.js
@@ -1,3 +1,15 @@
-// TODO: getAllTransactions()
-// TODO: filterTransactions()
-// TODO: sortTransactions() // see if possible through the front-end readymade chart
+import Transactions from "../../models/transactions/transaction.model.js"
+import getErrorMessages from "../errorMessages.js"
+
+
+async function readTransactions(req, res, next) {
+    try {
+        req.body.okMessage = 'Get transactions successful'
+        next()
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Unable to get transactions' })
+    }
+}
+
+export { readTransactions }
--- server/controllers/transactions/transactionHelper.js
+++ server/controllers/transactions/transactionHelper.js
@@ -5,4 +5,21 @@ const constructTransaction = ({ email, user, ...rest }) => {
     return { user_id: user._id, ...rest }
 }
 
-export { constructTransaction }
+async function getAllTransactions(req, res) {
+    try {
+        const transactions = await Transactions.find({ user_id: req.body.user._id })
+
+        const categories = await Transactions.distinct("category", { user_id: req.body.user._id })
+
+        res.status(200).json({
+            message: req.body.okMessage,
+            transactions: transactions,
+            categories: categories
+        })
+    }
+    catch (error) {
+        res.status(500).json({ message: getErrorMessages(error) })
+    }
+}
+
+export { constructTransaction, getAllTransactions }
