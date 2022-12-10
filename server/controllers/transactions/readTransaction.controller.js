f9899d217e3c23e6b6fca0d74b6f633e98ac1a27
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-10 05:52:28 +0530

    Add filter transaction controller

--- server/controllers/transactions/readTransaction.controller.js
+++ server/controllers/transactions/readTransaction.controller.js
@@ -1,5 +1,6 @@
 import Transactions from "../../models/transactions/transaction.model.js"
 import getErrorMessages from "../errorMessages.js"
+import { constructFilter } from "./transactionHelper.js"
 
 
 async function readTransactions(req, res, next) {
@@ -12,4 +13,27 @@ async function readTransactions(req, res, next) {
     }
 }
 
-export { readTransactions }
+async function filterTransactions(req, res) {
+    try {
+        // console.log(constructFilter(req.body));
+        // res.send('check log')
+
+        // const filter = await constructFilter(req.query)
+
+        const filter = await constructFilter(req.body)
+
+        const transactions = await Transactions.find(filter)
+
+        req.body.okMessage = 'Filter transactions successful'
+
+        res.status(200).json({
+            message: req.body.okMessage,
+            transactions: transactions,
+        })
+    }
+    catch (error) {
+        res.status(400).json({ message: getErrorMessages(error) })
+    }
+}
+
+export { readTransactions, filterTransactions }
--- server/controllers/transactions/transactionHelper.js
+++ server/controllers/transactions/transactionHelper.js
@@ -5,6 +5,61 @@ const constructTransaction = ({ email, user, ...rest }) => {
     return { user_id: user._id, ...rest }
 }
 
+const constructFilter = async ({ email, user, ...filters }) => {
+    const { startDate, endDate, startAmount, endAmount, type, categories, recurring } = filters
+
+    const filter = {};
+
+    filter.user_id = user._id;
+
+    (startDate || endDate) && (filter.date = {});
+    if (startDate && endDate && (startDate > endDate)) {
+        throw new Error('Start date cannot be greater than end date')
+    }
+    startDate && (filter.date.$gte = startDate);
+    endDate && (filter.date.$lte = endDate);
+
+    (startAmount || endAmount) && (filter.amount = {});
+    if (startAmount && endAmount && (startAmount > endAmount)) {
+        throw new Error('Start amount cannot be greater than end amount')
+    }
+    startAmount && (filter.amount.$gte = startAmount);
+    endAmount && (filter.amount.$lte = endAmount);
+
+    if (type) {
+        if (type === 'Spent' || type === 'Earned') {
+            filter.type = type
+        }
+        else {
+            throw new Error('Type can be Spent/Earned')
+        }
+    }
+
+    if (categories) {
+        const validCategories = await Transactions.distinct("category", { user_id: user._id })
+
+        if (categories.every(category => validCategories.includes(category))) {
+            filter.category = { $in: categories }
+        }
+        else {
+            throw new Error('Invalid category')
+        }
+    }
+
+    // categories
+    //     && categories.every(category => validCategories.includes(category))
+    //     && (filter.category = { $in: categories });
+
+    if (recurring) {
+        if (typeof recurring !== 'boolean') {
+            throw new Error('Recurring can be true or false')
+        }
+        filter.recurring = recurring
+    }
+
+    return filter
+}
+
 async function getAllTransactions(req, res) {
     try {
         const transactions = await Transactions.find({ user_id: req.body.user._id })
@@ -22,4 +77,4 @@ async function getAllTransactions(req, res) {
     }
 }
 
-export { constructTransaction, getAllTransactions }
+export { constructTransaction, constructFilter, getAllTransactions }
