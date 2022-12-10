2c5d145eac90455ad76146bd4d641f6ac8df226d
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-10 06:07:19 +0530

    Implement add transaction

--- client/src/transactions/crud/transactions.js
+++ client/src/transactions/crud/transactions.js
@@ -29,4 +29,39 @@ async function getTransactions(setIsLoading, setTransactions, setCategories) {
     setIsLoading(false)
 }
 
-export { getTransactions }
+async function saveTransaction(formValues, setIsLoading, setSelectedTransaction, setTransactionFormOpen, setTransactions, setCategories) {
+    setIsLoading(true)
+
+    if (!formValues.recurring && formValues.remind_after_days) {
+        formValues.remind_after_days = undefined
+    }
+
+    const response = await fetch('/api/transaction', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formValues)
+    })
+
+    const responseData = await response.json()
+
+    if (responseData.message === 'Transaction added successfully') {
+        setTransactions(responseData.transactions)
+
+        setCategories(makeCategoriesArray(responseData.categories))
+
+        setTransactionFormOpen(false)
+        setSelectedTransaction({})
+
+        setIsLoading(false)
+
+        message.success('Transaction added')
+    }
+    else {
+        setIsLoading(false)
+        message.error(responseData.message)
+    }
+}
+
+export { getTransactions, saveTransaction }
