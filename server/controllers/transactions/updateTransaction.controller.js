5eba964adf736fdc62dea5d20ffd2efe69abafe3
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-10 05:49:19 +0530

    Add transaction update controller

--- server/controllers/transactions/updateTransaction.controller.js
+++ server/controllers/transactions/updateTransaction.controller.js
@@ -1,1 +1,32 @@
-// TODO: editTransaction() - this will be the POST request function that will be passed to the express function -> app.patch('/transactions', editTransaction)
+import Transactions from "../../models/transactions/transaction.model.js"
+import getErrorMessages from "../errorMessages.js"
+import { constructTransaction } from "./transactionHelper.js"
+
+async function updateTransaction(req, res, next) {
+    try {
+        /* not using upsert because cast to ObjectId can fail because the user can give any incorrect id in any format */
+
+        const transaction = constructTransaction(req.body)
+
+        const updatedTransaction = await Transactions.replaceOne(
+            { _id: req.params.transactionId },
+            transaction,
+            { new: true /*, upsert: true */, runValidators: true }
+        )
+
+        if (!updatedTransaction) {
+            await Transactions.create(transaction)
+            req.body.okMessage = 'Transaction added successfully'
+        }
+        else {
+            req.body.okMessage = 'Transaction updated successfully'
+        }
+
+        next()
+    }
+    catch (error) {
+        res.status(400).json({ message: getErrorMessages(error) })
+    }
+}
+
+export { updateTransaction }
