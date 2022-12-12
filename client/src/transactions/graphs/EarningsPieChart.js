51a33f08ddd31c031d09e7a03ef2a6dcdfe24e7a
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-12 15:23:35 +0530

    Add earnings pie chart

--- client/src/transactions/graphs/EarningsPieChart.js
+++ client/src/transactions/graphs/EarningsPieChart.js
@@ -1,13 +1,35 @@
 import { Empty, Typography } from "antd";
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { getRandomBackgroundColors } from "./graphHelper";
+
+Chart.register(ArcElement, Tooltip, Legend);
+Chart.defaults.font.size = 16
+Chart.defaults.plugins.legend.align = 'start'
 
 const EarningsPieChart = ({ data }) => {
     if (data.categories.length === 0) {
         return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
     }
 
+    const backgroundColors = getRandomBackgroundColors(data.categories)
+
+    const pieChartData = {
+        labels: data.categories,
+        datasets: [
+            {
+                label: 'Earned',
+                data: data.earned,
+                backgroundColor: backgroundColors
+            }
+        ]
+    }
+
     return (
         <div className="analysis-container">
             <Typography.Title level={4}>Earnings Pie Chart</Typography.Title>
+
+            <Pie data={pieChartData} />
         </div>
     );
 }
