23a78cd48853e8fc3d9e4132f78bb5c33800df46
bhushan-it <84985241+bhushan-it@users.noreply.github.com>
2022-12-12 15:22:43 +0530

    Add spendings pie chart

--- client/src/transactions/graphs/SpendingsPieChart.js
+++ client/src/transactions/graphs/SpendingsPieChart.js
@@ -1,13 +1,36 @@
 import { Empty, Typography } from "antd";
+import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { getRandomBackgroundColors } from "./graphHelper";
+
+Chart.register(ArcElement, Tooltip, Legend);
+Chart.defaults.font.size = 16
+Chart.defaults.plugins.legend.align = 'start'
 
 const SpendingsPieChart = ({ data }) => {
     if (data.categories.length === 0) {
         return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
     }
 
+    const backgroundColors = getRandomBackgroundColors(data.categories)
+
+    const pieChartData = {
+        labels: data.categories,
+        datasets: [
+            {
+                label: 'Spent',
+                data: data.spent,
+                backgroundColor: backgroundColors
+            }
+        ],
+    }
+
+
     return (
         <div className="analysis-container" >
             <Typography.Title level={4}>Spendings Pie Chart</Typography.Title>
+
+            <Pie data={pieChartData} />
         </div >
     );
 }
